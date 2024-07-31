058e960cfa5744ecd2084f2480b4ae5e89f14e75
ClaudiaHlz <c.holzgethan@outlook.at>
2024-07-31 21:19:27 +0200

    Finished task 3 and 4

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -26,21 +26,63 @@ public_users.get('/isbn/:isbn',function (req, res) {
   if(books[isbn_req]){
     return res.status(200).send(JSON.stringify(books[isbn_req], null, 4));
   }else{
-    return res.status(400).send("ISBN does not exist!")
+    return res.status(400).send("ISBN does not exist!");
   }  
  });
   
 // TASK 3
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author_req = req.params.author;
+
+  //console.log(Object.entries(books))
+  let book_list = Object.entries(books);
+  let new_list = [];
+  // Option 1: manual filtering
+  /*book_list.forEach(([isbn, entry]) => {
+    if (entry.author == author_req){
+      new_list.push(entry);
+    }
+  });
+  console.log(new_list);*/
+
+  // Option 2: using .filter()
+  let filtered_list = book_list.filter((id) => id[1].author === author_req);
+  //console.log(filtered_list)
+
+  // return the filtered list
+  if(filtered_list){
+    //return res.status(200).send(JSON.stringify(filtered_list, null, 4));
+    return res.status(200).send(filtered_list);
+  }else{
+    return res.status(400).send("Author not found!");
+  }
 });
 
+// TASK 4
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const title_req = req.params.title;
+
+  //console.log(Object.entries(books))
+  let book_list = Object.entries(books);
+  let filtered_list = [];
+  // Option 1: manual filtering
+  book_list.forEach(([isbn, entry]) => {
+    if (entry.title == title_req){
+      filtered_list.push([isbn, entry]);
+    }
+  });
+  console.log(filtered_list);
+
+
+  // return the filtered list
+  if(filtered_list){
+    //return res.status(200).send(JSON.stringify(filtered_list, null, 4));
+    return res.status(200).send(filtered_list);
+  }else{
+    return res.status(400).send("Book title not found!");
+  }
 });
 
 //  Get book review
