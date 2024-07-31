352f2d29a083bc4624910f43a4830730de23f330
ClaudiaHlz <c.holzgethan@outlook.at>
2024-07-31 20:46:22 +0200

    finished task 1 and 2

--- final_project/index.js
+++ final_project/index.js
@@ -4,19 +4,42 @@ const session = require('express-session')
 const customer_routes = require('./router/auth_users.js').authenticated;
 const genl_routes = require('./router/general.js').general;
 
+//create express ap
 const app = express();
 
+// use express.json
 app.use(express.json());
 
+// stores the session data on the client within a cookie
+// secret ... used to sign the session ID cookie
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
+// authentication
 app.use("/customer/auth/*", function auth(req,res,next){
 //Write the authenication mechanism here
+    // check if a user is logged in and has a valid access token
+    if(req.session.authorization) {
+        let token = req.session.authorization['accessToken'];
+
+        //verify the token
+        jwt.verify(token, "access", (err, user) => {
+            if(!err){
+                req.user = user;
+                next();
+            }else{
+                return res.status(403).json({message: "This user is not authenticated!"});
+            }
+        });
+    } else {
+        return res.status(403).json({message: "User is not logged in!"});
+    }
 });
  
 const PORT =5000;
 
+//reroute all requests to these files
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
 
+// start app
 app.listen(PORT,()=>console.log("Server is running"));
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -10,18 +10,27 @@ public_users.post("/register", (req,res) => {
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
+// TASK 1
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+
+  return res.status(200).send(JSON.stringify(books, null, 4));
 });
 
+// TASK 2
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn_req = req.params.isbn;
+
+  if(books[isbn_req]){
+    return res.status(200).send(JSON.stringify(books[isbn_req], null, 4));
+  }else{
+    return res.status(400).send("ISBN does not exist!")
+  }  
  });
   
+// TASK 3
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
